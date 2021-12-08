1c40ae537d0d9dde6e3be5460e3de8af87d74f3e
Drew Winstel <drew@hsv.beer>
2021-12-08 06:29:36 -0600

    day 8: decoding 7-segment LEDs
    I could've gone with a more analytical approach that really digs in
    and tries to work out the possible solutions, but a brute force
    substitution cipher worked in 10.3 sec

---
+++ day08.py
@@ -0,0 +1,85 @@
+from typing import Iterable
+from itertools import permutations
+
+
+TEST_INPUT = """be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce""".splitlines()
+
+
+def part_one(puzzle: Iterable[str]) -> int:
+    count = 0
+    for line in puzzle:
+        _, output = line.split("|")
+        words = list(output.split())
+        count += sum(len(word) in {2, 3, 4, 7} for word in words)
+    return count
+
+
+def sub_out_words(words: Iterable[str], cipher: tuple[str]) -> list[str]:
+    output = []
+    for word in words:
+        new_word = ""
+        for char in word:
+            new_word += cipher[ord(char) - ord("a")]
+        output.append("".join(sorted(i for i in new_word)))
+    return output
+
+
+def decode_line(line: str) -> int:
+    expected_outputs = {
+        "abcefg": 0,
+        "cf": 1,
+        "acdeg": 2,
+        "acdfg": 3,
+        "bcdf": 4,
+        "abdfg": 5,
+        "abdefg": 6,
+        "acf": 7,
+        "abcdefg": 8,
+        "abcdfg": 9,
+    }
+    targeted_words = set(expected_outputs)
+    # oh, hey, this is a substitution cipher
+    # and we only have 10 inputs and outputs
+    # so we might be able to brute force it
+    perms = list(permutations("abcdefg"))
+    in_words, out_words = [i.strip().split() for i in line.split("|")]
+    for candidate in perms:
+        # 'abcdefg' should become 'deafgbc'
+
+        decoded_inputs = sub_out_words(in_words, candidate)
+        if set(decoded_inputs) == targeted_words:
+            decoded_output = sub_out_words(out_words, candidate)
+            return int("".join(str(expected_outputs[word]) for word in decoded_output))
+
+    raise ValueError(f"Did not get a winner from {line}")
+
+
+def part_two(puzzle: Iterable[str]) -> int:
+    return sum(decode_line(line) for line in puzzle)
+
+
+def main():
+    part_one_result = part_one(TEST_INPUT)
+    assert part_one_result == 26, part_one_result
+    part_two_test = [
+        "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf"
+    ]
+    part_two_result = part_two(part_two_test)
+    assert part_two_result == 5353, part_two_result
+    with open("day08.txt") as infile:
+        puzzle = [line.strip() for line in infile]
+    print(part_one(puzzle))
+    print(part_two(puzzle))
+
+
+if __name__ == "__main__":
+    main()
